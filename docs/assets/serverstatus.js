3fbc80cb1ff13ce1ab3954776613f24e0507dfdc
Josue Navarro <65885730+josuens@users.noreply.github.com>
2021-06-18 18:34:12 -0600

    Update serverstatus.js

--- docs/assets/serverstatus.js
+++ docs/assets/serverstatus.js
@@ -7,7 +7,7 @@
         document.getElementById("serverInd").classList.add("text-green-600");
         document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Juega ahora: ${data.players.online} jugadores</span>`;
         {% else %}
-        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Actualmente encendido!&ensp;&middot;&ensp;${data.players.online} jugadores</p>`;
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Juega ahora!&ensp;&middot;&ensp;${data.players.online} jugadores</p>`;
         document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Encendido, ${data.players.online} jugadores</p>`;
         {% endif %}
     }
