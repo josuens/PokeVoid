87eaaa6f3ad106991f2bf3be9fc359c8bab72331
Josue Navarro <65885730+josuens@users.noreply.github.com>
2021-06-18 18:28:44 -0600

    Update serverstatus.js

--- docs/assets/serverstatus.js
+++ docs/assets/serverstatus.js
@@ -5,18 +5,18 @@
     if (data.online == true) {
         {% if site.siteNav %}
         document.getElementById("serverInd").classList.add("text-green-600");
-        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Play Now: ${data.players.online} Online</span>`;
+        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Juega ahora: ${data.players.online} jugadores</span>`;
         {% else %}
-        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;We're online!&ensp;&middot;&ensp;${data.players.online} playing</p>`;
-        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Online, ${data.players.online} playing</p>`;
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Actualmente encendido!&ensp;&middot;&ensp;${data.players.online} jugadores</p>`;
+        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Encendido, ${data.players.online} jugadores</p>`;
         {% endif %}
     }
     else {
         {% if site.siteNav %}
         document.getElementById("serverInd").classList.add("text-red-600");
         {% else %}
-        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Sorry! We're offline...</p>`;
-        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Offline</p>`;
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Lo siento! Server apagado...</p>`;
+        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Desconectado</p>`;
         {% endif %}
     }
 });
